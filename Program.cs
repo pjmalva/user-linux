eb0d37ac5aad4ca730c010adbf5ed747f1812ba8
Pj Malva <pjmalva@mitrix-dev-03.localdomain>
2019-03-27 10:25:18 -0300

    Initial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace user_linux
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
